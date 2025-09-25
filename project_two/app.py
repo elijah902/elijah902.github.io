d2f96b7068034e38606669a50101b6cd8c05ff3c
elijah902 <elijahajjones@gmail.com>
2025-09-25 13:34:37 -0400

    added project two + flask backend and requirements

---
+++ project_two/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, redirect, request, session, url_for
+from spotipy.oauth2 import SpotifyOAuth
+import os
+
+app = Flask(__name__)
+app.secret_key = "my_secret_key"
+app.config['SESSION_COOKIE_NAME'] = 'spotify-login'
+
+CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
+SCOPE = "user-library-read playlist-read-private"
+
+sp_oauth = SpotifyOAuth(
+    client_id=CLIENT_ID, 
+    client_secret=CLIENT_SECRET, 
+    redirect_uri=REDIRECT_URI, 
+    scope=SCOPE
+)
+
+@app.route('/')
+def login():
+    auth_url = sp_oauth.get_authorize_url()
+    return f'<a href="{auth_url}">Login with Spotify</a>'
+
+@app.route('/callback')
+def callback():
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session['token_info'] = token_info
+    return redirect(url_for('me'))
+
+@app.route('/me')
+def me():
+    token_info = session.get('token_info', None)
+    if not token_info:
+        return redirect("/")
+    import spotipy
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    me = sp.current_user()
+    return f"Hello, {me['display_name']}!"
