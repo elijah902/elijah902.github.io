2096c85822618e22a6237e8b52fc06f2606ec332
Elijah <161072921+elijah902@users.noreply.github.com>
2025-09-28 17:46:13 -0400

    Update app.py

--- project_two/app.py
+++ project_two/app.py
@@ -9,7 +9,7 @@ app.config['SESSION_COOKIE_NAME'] = 'spotify-login'
 CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 REDIRECT_URI = os.getenv('REDIRECT_URI')
-SCOPE = "user-library-read playlist-read-private"
+SCOPE = "user-library-read playlist-read-private playlist-read-collaborative"
 
 sp_oauth = SpotifyOAuth(
     client_id=CLIENT_ID, 
